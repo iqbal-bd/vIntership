a9e72d48e8eeda726dcd15996930e0787b792784
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-06 07:19:24 +0600

    ProductTypes UI created with post method

---
+++ EcommerceWebsite/EcommerceWebsite/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,45 @@
+﻿using EcommerceWebsite.Data;
+using EcommerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+
+        private ApplicationDbContext _db;
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {   
+            var data = _db.ProductTypes.ToList();
+            return View(data);
+        }
+
+        //Get Method
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(productTypes);
+        }
+    }
+}
