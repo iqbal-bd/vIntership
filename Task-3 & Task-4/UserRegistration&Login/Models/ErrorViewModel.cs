94b7894ae5fddfa544145d98cd398cd980d1b653
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-08 03:47:59 +0600

    project setup

---
+++ Task-3 & Task-4/UserRegistration&Login/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserRegistration_Login.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
