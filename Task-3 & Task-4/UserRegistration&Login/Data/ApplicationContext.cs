f9289a2ed4fc1db29d6c7e9743ef7f7a1d346a78
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-08 06:29:02 +0600

    database connect with migration

---
+++ Task-3 & Task-4/UserRegistration&Login/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserRegistration_Login.Models;
+
+namespace UserRegistration_Login.Data
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Task-3 & Task-4/UserRegistration&Login/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistration_Login.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string District { get; set; }
+        public string Division { get; set; }
+
+    }
+}
