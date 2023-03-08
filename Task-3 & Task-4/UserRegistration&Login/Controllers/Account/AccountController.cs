0ba9b1b042150b656e4926dda45f7f46ad545110
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-09 05:52:41 +0600

    singup and login design completed

--- Task-3 & Task-4/UserRegistration&Login/Controllers/Account/AccountController.cs
+++ Task-3 & Task-4/UserRegistration&Login/Controllers/Account/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UserRegistration_Login.Models.ViewModel;
 
 namespace UserRegistration_Login.Controllers.Account
 {
@@ -9,7 +10,24 @@ namespace UserRegistration_Login.Controllers.Account
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login_SignUp model)
+        {
+            return View();
+        }
+
         public IActionResult SignUP()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUP(Login_SignUp model) 
         {
             return View();
         }
