59fb1efda0f4aac5db3919eb3d291f80b6429bdc
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-19 15:56:06 +0600

    post method added

---
+++ Asp.Net Core Razor pages CRUD/Blog/BlogWebApp/Pages/Admin/Blogs/Add.cshtml.cs
@@ -0,0 +1,43 @@
+using BlogWebApp.Data;
+using BlogWebApp.Models.Domain;
+using BlogWebApp.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogWebApp.Pages.Admin.Blogs
+{
+    public class AddModel : PageModel
+    {
+        private readonly BlogDbContext blogDbContext;
+
+        [BindProperty]
+        public AddBlogPost AddBlogPostRequest { get; set; }
+
+        public AddModel(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            var blogPost = new BlogPost()
+            {
+                Heading = AddBlogPostRequest.Heading,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                Content = AddBlogPostRequest.Content,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = AddBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                Author = AddBlogPostRequest.Author,
+                Visible = AddBlogPostRequest.Visible,
+            };
+
+            blogDbContext.BlogPosts.Add(blogPost);
+            blogDbContext.SaveChanges();
+        }
+    }
+}
