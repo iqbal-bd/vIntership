e4b58a358c551dfd133bf2bbd256019ade952de9
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-20 14:24:16 +0600

    Api Get method successful

---
+++ Task-1 & Task-2/StudentProfile page with Rest Api/StudentProfile/StudentProfileRestApi/Controllers/StudentAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentProfileRestApi.Models;
+
+namespace StudentProfileRestApi.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class StudentAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student { Id = 1,Name = "Kamran",Role = 101,Phone=01722233445},
+                new Student { Id = 2,Name = "Ashraf",Role = 102,Phone=01344455666}
+            };
+        }
+
+    }
+}
---
+++ Task-1 & Task-2/StudentProfile page with Rest Api/StudentProfile/StudentProfileRestApi/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentProfileRestApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Role { get; set; }
+        public int Phone { get; set; }
+    }
+}
