50fda1e4f7c5d922fb576a9f658ef1f431f9f8ef
iqbal <iqbal.hossainnbd@gmail.com>
2023-03-30 00:11:44 +0600

    crud added

---
+++ Asp.Net Core Projects/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+
+        } 
+
+        public IActionResult Welcome(string name, int id = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["Id"] = id;
+            return View();
+        }
+    }
+}
