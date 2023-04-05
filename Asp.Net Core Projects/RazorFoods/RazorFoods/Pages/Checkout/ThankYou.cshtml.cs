9c825ffd1117a2406cc78b71a65cf6880306e9b4
iqbal <iqbal.hossainnbd@gmail.com>
2023-04-05 22:49:10 +0600

    checkout and forms created

---
+++ Asp.Net Core Projects/RazorFoods/RazorFoods/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorFoods.Pages.Checkout
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
