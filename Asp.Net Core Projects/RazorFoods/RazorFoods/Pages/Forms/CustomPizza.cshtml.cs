90fb5c65fde07be5b77a20d843f6503b2d15cfe1
iqbal <iqbal.hossainnbd@gmail.com>
2023-04-07 01:16:59 +0600

    custom pizza UI created

--- Asp.Net Core Projects/RazorFoods/RazorFoods/Pages/Forms/CustomPizza.cshtml.cs
+++ Asp.Net Core Projects/RazorFoods/RazorFoods/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorFoods.Models;
 
 namespace RazorFoods.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
+
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 5;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.beef) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
     }
 }
